63107155381bbc2e2cba8720b2310334d51ea0a6
Robert Hallatt <roberth@cps.ca>
2025-10-01 13:27:11 -0400

    refactor: simplify Vite asset loading and update dev server configuration

--- config/vite.php
+++ config/vite.php
@@ -3,11 +3,11 @@
 use craft\helpers\App;
 
 return [
-    'devServerPublic' => 'VITE_DEV_SERVER_PUBLIC_PLACEHOLDER',
+    'devServerPublic' => 'http://localhost:3030',
     'serverPublic' => '/',
     'useDevServer' => in_array(App::env('CRAFT_DEV_MODE'), [true, 'true', '1', 1], true),
     'manifestPath' => '@webroot/.vite/manifest.json',
-    'devServerInternal' => 'VITE_DEV_SERVER_INTERNAL_PLACEHOLDER',
+    'devServerInternal' => 'http://localhost:3030',
     'checkDevServer' => true,
     'errorEntry' => '',
     'includeReactRefreshShim' => false,
