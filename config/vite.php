71c6bcd78fa46edb2136fae54c73521ee7a48d96
Robert Hallatt <roberth@cps.ca>
2025-10-01 14:14:21 -0400

    fix: simplify dev server detection and add debug info for Vite configuration

--- config/vite.php
+++ config/vite.php
@@ -5,8 +5,8 @@ use craft\helpers\App;
 return [
     'devServerPublic' => 'http://localhost:3000',
     'serverPublic' => '/',
-    'useDevServer' => in_array(App::env('CRAFT_DEV_MODE'), [true, 'true', '1', 1], true),
-    'manifestPath' => '@webroot/.vite/manifest.json',
+    'useDevServer' => App::env('CRAFT_DEV_MODE') == 'true',
+    'manifestPath' => '@webroot/manifest.json',
     'devServerInternal' => 'http://localhost:3000',
     'checkDevServer' => true,
     'errorEntry' => '',
