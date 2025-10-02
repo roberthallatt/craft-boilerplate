b3cbbb2055f6c64bb14933be96e19344e77a62db
Robert Hallatt <roberth@cps.ca>
2025-10-02 12:21:17 -0400

    feat: always check Vite dev server and improve development mode asset loading

--- config/vite.php
+++ config/vite.php
@@ -7,10 +7,10 @@ $isDev = App::env('CRAFT_DEV_MODE') === 'true' || App::env('CRAFT_DEV_MODE') ===
 return [
     'devServerPublic' => App::env('VITE_DEV_SERVER_PUBLIC') ?: 'http://localhost:3000',
     'serverPublic' => '/',
-    'useDevServer' => $isDev, // Use dev server only in development
+    'useDevServer' => $isDev,
     'manifestPath' => '@webroot/.vite/manifest.json',
     'devServerInternal' => App::env('VITE_DEV_SERVER_INTERNAL') ?: 'http://host.docker.internal:3000',
-    'checkDevServer' => $isDev, // Only check dev server in development
+    'checkDevServer' => true, // Always check if dev server is running
     'errorEntry' => '',
     'includeReactRefreshShim' => false,
     'includeModulePreloadShim' => true,
